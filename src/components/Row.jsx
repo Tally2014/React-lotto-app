2672a70a6f0665e0b02da4a986798ca128ac82f7
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-27 17:03:46 +0200

    Create Row Component more dynamic and logically complete

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Ball from './Ball';
 import './Row.css';
 import { getRolls } from '../utils';
 
-export default function Row() {
-    const [balls, setBalls] = useState(getRolls(6-1,52)); /*Generate Random Balls*/
-    const [bonus, setBonus] = useState(getRolls(1,20)); /*Generate Bonus Ball*/
-    console.log(bonus);
+export default function Row({numbers=6, isBonus=true, maxNumber=52, bonusMax=20}) {
+    
+    const [balls, setBalls] = useState([]); 
+    const [bonus, setBonus] = useState(getRolls(1,bonusMax)); /*Generate Bonus Ball*/
+
+    useEffect(() => {
+        const fields = isBonus ? numbers - 1 : numbers;
+        setBalls(getRolls(fields, maxNumber)); /*Generate Random Balls*/
+      }, [numbers, isBonus]);
+
   return (
     <section className='Row'>
         {balls.map((v,i) => <Ball key={i} val={v}/>) /*Render Random Balls*/} 
-        <Ball val={bonus} color={'#006CA5'} textColor={'#02367b'}/>  {/*Render Random Balls*/}
+        {isBonus && <Ball val={bonus} color={'#006CA5'} textColor={'#02367b'}/> /*Render Random Balls*/}
     </section>
   )
 }
