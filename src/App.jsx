4b2e700217eff1c3749c7cf80d6f7c6224c27917
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-28 00:53:03 +0200

    Add sections for main component for settings panel and main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css';
 import Board from './components/Board';
+import reactLogo from './assets/react.svg'
 
 function App() {
 
   return (
     <>
-      <h1>Lotto-Gen</h1>
-      <Board/>
+    <nav>
+    <div className='navbar-container'>
+        <img src={reactLogo} className='Logo' alt='React Logo'/>
+        <h2>Lotto-Gen</h2>
+      </div>
+    </nav>
+    <main>
+      <div className='container'>
+        <div className='left'>
+
+        </div>
+        <div className='middle'>
+          <Board/>
+          <Board/>
+        </div>
+      </div>
+      
+    </main>
+      
     </>
   )
 }
