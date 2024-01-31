de055b7243b859f87bea0d60aa11a31f796f5a7e
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-31 06:31:09 +0200

    Move App HTML Markup to Board.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import './App.css';
 import Board from './components/Board';
 import reactLogo from './assets/react.svg';
-import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [configurations, setConfigurations] = useState({
-    rows: 6,
-    maxRandomNumber: 52,
-    includeBonusBall: true,
-    maxBonusNumber: 20,
-    boards: 2
-  });
-
-  const handleConfigChange = (key, value) => {
-    setConfigurations((prevConfig) => ({
-      ...prevConfig,
-      [key]: value,
-    }));
-  };
 
 
   return (
@@ -31,46 +16,8 @@ function App() {
     </nav>
     <main>
       <section className='container'>
-        <section className='left'>
-          <div className='randomNum'>
-            <h2>
-              Configurations
-            </h2>
-
-          </div>
-            <div className='numConainer'>
-              <div>
-                <p>Rows:</p>
-                <input type='number' value={configurations.rows}
-                  onChange={(e) => handleConfigChange('rows', parseInt(e.target.value))}/>
-              </div>
-              <div>
-                <p>Max Random #:</p>
-                <input type='number' value={configurations.maxRandomNumber}
-                  onChange={(e) => handleConfigChange('maxRandomNumber', parseInt(e.target.value))}/>
-              </div>
-              <div>
-                <p>Bonus Ball</p>
-                <input type="checkbox" checked={configurations.includeBonusBall}
-                  onChange={(e) => handleConfigChange('includeBonusBall', e.target.checked)}/>
-              </div>
-              <div>
-                <p>Max bonus #:</p>
-                <input type='number' value={configurations.maxBonusNumber}
-                  onChange={(e) => handleConfigChange('maxBonusNumber', parseInt(e.target.value))}
-                />
-              </div>
-              <div>
-                <p>No. of Boards:</p>
-                <input type='number' value={configurations.boards}
-                  onChange={(e) => handleConfigChange('boards', parseInt(e.target.value))}
-                />
-              </div>
-            </div>
-            <button>Generate</button>
-        </section>
         <section className='middle'>
-          <Board settings={configurations}/>
+          <Board/>
         </section>
       </section>
       
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,73 @@
-import Row from "./Row"
+import Row from "./Row";
+import { useState, useEffect } from 'react';
 
-export default function Board({settings}) {
+export default function Board() {
     
-    const {rows, maxRandomNumber, includeBonusBall, maxBonusNumber, boards} = settings;
+  const [configurations, setConfigurations] = useState({
+    rows: 6,
+    maxRandomNumber: 52,
+    includeBonusBall: true,
+    maxBonusNumber: 20,
+    boards: 2
+  });
+
+  const handleConfigChange = (key, value) => {
+    setConfigurations((prevConfig) => ({
+      ...prevConfig,
+      [key]: value,
+    }));
+  };
+
+
+    //const {rows, maxRandomNumber, includeBonusBall, maxBonusNumber, boards} = settings;
+
     
     // Create an array with 'boards' number of elements and map each element to a Row component
-    const rowComponents = [...Array(boards)].map((_, index) => <Row key={index} />);
+    const rowComponents = [...Array(configurations.boards)].map((_, index) => <Row key={index} />);
 
   return (
     <div>
-        {rowComponents}
+        <section className='left'>
+          <div className='randomNum'>
+            <h2>
+              Configurations
+            </h2>
+
+          </div>
+            <div className='numConainer'>
+              <div>
+                <p>Rows:</p>
+                <input type='number' value={configurations.rows}
+                  onChange={(e) => handleConfigChange('rows', parseInt(e.target.value))}/>
+              </div>
+              <div>
+                <p>Max Random #:</p>
+                <input type='number' value={configurations.maxRandomNumber}
+                  onChange={(e) => handleConfigChange('maxRandomNumber', parseInt(e.target.value))}/>
+              </div>
+              <div>
+                <p>Bonus Ball</p>
+                <input type="checkbox" checked={configurations.includeBonusBall}
+                  onChange={(e) => handleConfigChange('includeBonusBall', e.target.checked)}/>
+              </div>
+              <div>
+                <p>Max bonus #:</p>
+                <input type='number' value={configurations.maxBonusNumber}
+                  onChange={(e) => handleConfigChange('maxBonusNumber', parseInt(e.target.value))}
+                />
+              </div>
+              <div>
+                <p>No. of Boards:</p>
+                <input type='number' value={configurations.boards}
+                  onChange={(e) => handleConfigChange('boards', parseInt(e.target.value))}
+                />
+              </div>
+            </div>
+            <button>Generate</button>
+        </section>
+        <section>
+          {rowComponents}
+        </section>      
     </div>
   )
 }
